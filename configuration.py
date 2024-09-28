db0244934fd3466f7b0cff984d3e87104c9130d0
Santiago Sánchez <san-san-05@hotmail.com>
2024-09-28 16:57:35 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = # Almacena la ruta para crear un kit en esta variable
-axvavavva
+oe9r8343ihih
