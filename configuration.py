e97178b0009cef649883b7d116b8d62ef06eda7c
Santiago Sánchez <san-san-05@hotmail.com>
2024-09-28 17:57:42 -0500

    Hi

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
-oe9r8343ihih
+URL_SERVICE = "# Inserta tu dirección de URL sin la barra diagonal al final"
+CREATE_USER_PATH = "/api/v1/users"
+KITS_PATH = "/api/v1/kits"
+dfgjej
