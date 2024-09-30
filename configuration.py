fef8c965cb22e23aaf895e63b3feda4275447cdd
Santiago Sánchez <san-san-05@hotmail.com>
2024-09-29 23:20:59 -0500

    Hi

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "# Inserta tu dirección de URL sin la barra diagonal al final"
+URL_SERVICE = "https://cnt-dbd6340c-b533-4ed7-b7ef-61b11e91d8c8.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
---
+++ data.py
@@ -0,0 +1,9 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
