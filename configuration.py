10f56283c3bb98243f7f755fb517dcb48a74da49
Santiago Sánchez <san-san-05@hotmail.com>
2024-10-02 00:16:29 -0500

    Proyecto

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-da268364-419f-4fd5-a621-8c68eb834f65.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-97404739-f846-47a0-8cc2-d3075318fcaa.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
