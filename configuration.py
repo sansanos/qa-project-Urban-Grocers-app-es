f6852c7179904c87b05b8528159be97323ae7a7c
Santiago Sánchez <san-san-05@hotmail.com>
2024-10-03 16:45:36 -0500

    Proyecto_Correcciones_2

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-6a74d002-4f85-48d7-af8d-a76f7e268161.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-a57ac6aa-61d4-4429-b197-541b49fb2862.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -23,53 +23,44 @@ def negative_assert(kit_body):
 
 # Prueba 1. En el parámetro name el número permitido de caracteres (1)
 def test_numero_permitido_caracteres_1():
-    kit_body = get_kit_body(data.kit_body_one_letter)
-    positive_assert(kit_body)
+    positive_assert(data.kit_body_one_letter)
 
 
 # Prueba 2. El número permitido de caracteres (511)
 def test_numero_permitido_caracteres_511():
-    kit_body = get_kit_body(data.kit_body_511_letters)
-    positive_assert(kit_body)
+    positive_assert(data.kit_body_511_letters)
 
 
 # Prueba 3. El número de caracteres es menor que la cantidad permitida (0)
 def test_numero_no_permitido_caracteres_0():
-    kit_body = get_kit_body(data.kit_body_no_letter)
-    negative_assert(kit_body)
+    negative_assert(data.kit_body)
 
 
 # Prueba 4. El número de caracteres es menor que la cantidad permitida (0)
 def test_numero_no_permitido_caracteres_512():
-    kit_body = get_kit_body(data.kit_body_512_letters)
-    negative_assert(kit_body)
+    negative_assert(data.kit_body_512_letters)
 
 
 # Prueba 5. Se permiten caracteres especiales
 def test_permite_caracteres_especiales():
-    kit_body = get_kit_body(data.kit_body_caracteres_especiales)
-    positive_assert(kit_body)
+    positive_assert(data.kit_body_caracteres_especiales)
 
 
 # Prueba 6. Se permiten espacios
 def test_permite_espacios():
-    kit_body = get_kit_body(data.kit_body_espacios)
-    positive_assert(kit_body)
+    positive_assert(data.kit_body_espacios)
 
 
 # Prueba 7. Se permiten números
 def test_permite_numeros():
-    kit_body = get_kit_body(data.kit_body_numeros)
-    positive_assert(kit_body)
+    positive_assert(data.kit_body_numeros)
 
 
 # Prueba 8. El parámetro no se pasa en la solicitud
 def test_no_se_pasa_parametros():
-    kit_body = get_kit_body(data.kit_body_sin_parametro)
-    negative_assert(kit_body)
+    negative_assert(data.kit_body_sin_parametro)
 
 
 # Prueba 9. Se ha pasado un tipo de parámetro diferente (número)
 def test_parametro_diferente_numero():
-    kit_body = get_kit_body(data.kit_body_parametro_diferente)
-    negative_assert(kit_body)
+    negative_assert(data.kit_body_parametro_diferente)
